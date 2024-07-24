f85886b5b99c5e0c619ec00bd1dd1bf437a33b9c
misskay535 <167699439+MissKay535@users.noreply.github.com>
2024-07-24 15:30:46 -0400

    Created all tables, imported CSVs, and added individual table images

---
+++ Employees_schema.sql
@@ -0,0 +1,52 @@
+CREATE TABLE titles(
+	title_id VARCHAR(5) PRIMARY KEY NOT NULL,
+	title VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE employees(
+	emp_no INT PRIMARY KEY NOT NULL,
+	emp_title_id VARCHAR(5) NOT NULL,
+	birth_date date NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	sex VARCHAR(10) NOT NULL,
+	hire_date date NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+
+CREATE TABLE departments(
+	dept_no VARCHAR(5) PRIMARY KEY NOT NULL,
+	dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(5) NOT NULL,
+	PRIMARY KEY(emp_no, dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE dept_manager(
+	dept_no VARCHAR(5) NOT NULL,
+	emp_no INT NOT NULL,
+	PRIMARY KEY(dept_no, emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE salaries(
+	emp_no INT PRIMARY KEY  NOT NULL,
+	salary DEC NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+-- Import each CSV file into its corresponding SQL table
+-- Display each table to confirm data is imported properly
+SELECT * FROM Titles;
+SELECT * FROM Employees;
+SELECT * FROM Departments;
+SELECT * FROM Dept_Emp;
+SELECT * FROM Dept_Manager;
+SELECT * FROM Salaries;
